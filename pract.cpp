88c1e237cfa22e85fab472431dfdff7793dfbafc
Gautam Waghela <waghelagautam752@gmail.com>
2026-01-10 14:58:24 +0530

    added frnd funtion

--- pract.cpp
+++ pract.cpp
@@ -1,10 +1,27 @@
 #include<iostream>
 using namespace std;
-
+class getclass{
+        int a,b;
+        public:
+        void getnum(){
+            cout<<"Enter the value of a:";
+            cin>>a;
+            cout<<"Enter the value of b:";
+            cin>>b;
+        };
+        void printnum(){
+            cout<<"a="<<a<<"b="<<b;
+        
+        };
+        friend void sum(getclass obj);
+    };
+void sum(getclass obj){
+    cout<<"The addtion of this 2 number is:"<<obj.a+obj.b<<endl;
+};
 int main(){
-    int a=1, b =28938,c;
-    cout<<"Maximum is :"<<((a<b)?b:a)<<endl;
-    
-
+    getclass obj1;
+    obj1.getnum();
+    obj1.printnum();
+    sum(obj1);
     return 0;
 }
